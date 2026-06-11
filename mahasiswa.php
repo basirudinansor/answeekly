d07bdbdb7af2794acbce4a99a708d3782d9f58ce
basirudinansor <anzhrohmm@gmail.com>
2026-06-11 11:59:07 +0700

    fix: table mahasiswa

--- mahasiswa.php
+++ mahasiswa.php
@@ -12,48 +12,48 @@
     <table border="1" align="center" cellspacing="0" cellpadding="10px">
         <tr>
             <td>
-                <a href="index.html">Home</a>
+                <a href="index.php">Home</a>
             </td>
             <td>
-                <a href="profile.html">Profile</a>
+                <a href="profile.php">Profile</a>
             </td>
             <td>
-                <a href="contact.html">Contact</a>
+                <a href="contact.php">Contact</a>
             </td>
             <td>
-                <a href="mahasiswa.html">Data Mahasiswa</a>
+                <a href="mahasiswa.php">Data Mahasiswa</a>
             </td>
         </tr>
     </table>
     <h2>
         Data Mahasiswa
     </h2>
-    <a href="tambahdata.html">
+    <a href="tambahdata.php">
         <button>Tambah Data</button>
     </a>
     <table border="1" cellpadding="5px">
         <tr>
-            <th rowspan="2">No</th>
-            <th rowspan="2">Nama</th>
-            <th rowspan="2">NIM</th>
-            <th rowspan="2">Foto</th>
-            <th colspan="3">Nilai</th>
-            <!-- <td>Baris 1, Kolom 2</td> -->
-        </tr>
-        <tr>
-            <!-- <td>Baris 2, Kolom 1</td> -->
-            <th>UTS</th>
-            <th>UAS</th>
-            <th>Tugas</th>
-        </tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Jurusan</th>
+            <th>Email</th>
+            <th>No. HP</th>
+            <th>Foto</th>
+            <th>Aksi</th>            
+        </tr>       
         <tr>
             <td align="center">1</td>
-            <td>M. Zulfikar Dzulkarnain Dzulhijah</td>
+            <td>M. Zulfikar Dzulkarnain</td>
             <td>3322114545</td>
-            <td><img src="assets/images/foto.jpg" width="70px" /></td>
-            <td align="center">90</td>
-            <td align="center">85</td>
-            <td align="center">50</td>
+            <td align="center">Teknologi Informasi</td>
+            <td align="center">zul@gmail.com</td>
+            <td align="center">0857869776543</td>
+            <td><img src="assets/images/foto.jpg" width="70px" /></td>  
+            <td>
+                <a href="editdata.php"><button>Edit</button></a> | 
+                <a href="deletedata.php"><button>Hapus</button></a>
+            </td>          
         </tr>
     </table>
     <hr/>
